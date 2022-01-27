9bda05fe9d9e0d427f02c5b0f9aaf481a3b9dda0
daliawk <63318701+daliawk@users.noreply.github.com>
2022-01-27 17:37:37 +0100

    Added Tree Health Bar

---
+++ Assets/TreeHealth.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+public class TreeHealth : MonoBehaviour
+{
+    Slider slider;
+    public bool hit;
+    public float rate = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        slider.value = slider.maxValue;
+        hit = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hit)
+        {
+            if(slider.value != slider.minValue)
+            {
+                slider.value -= rate * Time.deltaTime;
+            }
+        }
+    }
+
+    public void Burn()
+    {
+        hit = true;
+    }
+
+    public void StopBurn()
+    {
+        hit = false;
+    }
+
+}
